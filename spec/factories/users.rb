3fa2bb5db2fb8184f7044c07101a7086bd790f0e
lscantillo <luis.cantillo@wizeline.com>
2022-04-11 18:33:35 -0500

    Feat: users spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
-    username { "MyText" }
-    salt { "MyText" }
-    role { "" }
+    username { Faker::Internet.username(specifier: 5..8) }
+    salt { Faker::Lorem.characters(number: 10) }
+    password { Faker::Internet.password(min_length: 6, max_length: 16) }
+    password_confirmation { password }
+    role { "viewer" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid factory' do
+    it { expect(FactoryBot.build(:user)).to be_valid }
+  end
+  describe 'Validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
 end
